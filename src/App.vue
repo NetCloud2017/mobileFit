3c76c24ad50c995678255f58a771774750cf075e
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2022-08-17 11:07:40 +0800

    移动端适配方案

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+.logo {
+  width: 100px;
+  height: 100px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
+
+    <div>1 px 边框</div>
+    <div class="hairline"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-size: 16px;
+  font-weight: 550;
+}
+
+a {
+  color: #42b983;
+  font-size: 14px;
+}
+p {
+  font-size: 16px;
+}
+.hairline {
+  border-top: 1px solid #000;
+  width: 100vw;
+}
+</style>
